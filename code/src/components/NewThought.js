9a1eb929259fa4a1711d58b14bc0d29f408b6e8b
Birgit Nehrwein <nehrwein@posteo.de>
2021-10-29 15:56:36 +0200

    fixed responsiveness

--- code/src/components/NewThought.js
+++ code/src/components/NewThought.js
@@ -9,18 +9,18 @@ const NewThought = (props) => {
         onSubmit={props.handleFormSubmit}>
           <label>What's making you happy right now?
             <textarea 
-              rows={2} 
-              cols={40}
               value={props.newThought}
               onChange={event => props.setNewThought(event.target.value)}
             >
             </textarea>
           </label>
-        <span 
-          className={props.newThought.length > 140 ? "red" : ""}
-          >{props.newThought.length}/140</span>  
 
-        <span className="red">{
+        <span 
+          className={props.newThought.length > 140 ? "red" : "letter-amount"}>
+            {props.newThought.length}/140
+        </span>    
+        
+        <span className="alert red">{
           props.error === "required" ? "Please type a thought!" :
           props.error === "minlength" ? "Type more than 5 letters" :
           props.error === "maxlength" ? "Please type less than 140 letters" : ""}</span>  
@@ -30,6 +30,7 @@ const NewThought = (props) => {
           type={"submit"}
           text={<>❤️ Send Happy Thought ❤️</>}
         />
+
       </form>
     </div>
   )
