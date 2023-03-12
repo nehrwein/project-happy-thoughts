f49731cd60650ceff4085ae8fb115b8294151543
Birgit <birgit@hey.com>
2023-03-12 13:00:11 +0100

    updated api

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://nehrwein-happy-thoughts-api.herokuapp.com/thoughts'
-export const LIKES_API_URL = (id) => `https://nehrwein-happy-thoughts-api.herokuapp.com/thoughts/${id}/like`
+export const API_URL = 'https://project-happy-thoughts-api.onrender.com/thoughts'
+export const LIKES_API_URL = (id) => `https://project-happy-thoughts-api.onrender.com/thoughts/${id}/like`
